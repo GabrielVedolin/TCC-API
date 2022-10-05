cde2f978ba4435d604517602aa9cf760005a7c0a
Gabriel Vedolin <gabrielvedolindonascimento63@gmail.com>
2022-10-04 23:03:41 -0300

    Inserindo dados na tabela ultimo_feed

--- API/app.py
+++ API/app.py
@@ -48,113 +48,61 @@ def recomendacao(user_id, user_tipo):
         return jsonify(conteudosProf)
     else:
 
-        query = 'select * from ( '
-        query += 'select qp.user_id_aprendiz,qp.tipo_alternativa,sum(qp.peso) as somaDosPesos '
-        query += 'from shae_db.questionario_pedagogico qp '
-        query += 'group by user_id_aprendiz, tipo_alternativa '
-        query += ') as soma_resp_questionario where user_id_aprendiz = {0}'.format(user_id)
+    ############################### Motor de Adaptação - 1º Acesso - Questionário #######################
+    #####################################################################################################
 
-        cursor.execute(query)
+        qt_conteudo_feed = 16
 
-        respQuestionario = cursor.fetchall()
-        somaPesos = 0
+        query_quest = 'select * from ( '
+        query_quest += 'select qp.user_id_aprendiz,qp.tipo_alternativa,sum(qp.peso) as somaDosPesos '
+        query_quest += 'from shae_db.questionario_pedagogico qp '
+        query_quest += 'where id in (1,15,16,17,18,19,20,21,22)'
+        query_quest += 'group by user_id_aprendiz, tipo_alternativa '
+        query_quest += ') as soma_resp_questionario where user_id_aprendiz = {0}'.format(user_id)
 
-        for indice in respQuestionario:
-            print(indice)
-            somaPesos += indice[2]
+        dfquest = pd.read_sql(query_quest, conn)
 
-        # // (4, 'texto', 4)
-        # // (4, 'teste', 3)
-        # // (4, 'audio', 2)
-        # // (4, 'video', 1)
-        qtdConteudo = [['texto', 0.0], ['teste', 0.0], ['audio', 0.0], ['video', 0.0]]
-        print(somaPesos)
-        print(qtdConteudo)
-        indice = 0
+        dfquest.columns = ["user_id_aprendiz", "tipo_alternativa", "peso"]
 
-        for x in respQuestionario:
-            print(qtdConteudo[indice])
-            qtdConteudo[indice][1] = x[2] / somaPesos
-            indice += 1
+        dfquest['percent_peso'] = dfquest.peso / (dfquest.peso.sum())
 
-        print(qtdConteudo)
-        ##[['texto', 0.4],
-        #  ['teste', 0.3],
-        #  ['audio', 0.2],
-        #  ['video', 0.1]]
+        dfquest['prox_feed'] = dfquest.percent_peso * qt_conteudo_feed
 
-        nTotalConteudos = 20
-        indice = 0
-        for y in qtdConteudo:
-            qtdConteudo[indice][1] = y[1] * nTotalConteudos
-            indice += 1
+        dfquest['prox_feed'] = round(dfquest['prox_feed'])
 
-        print(qtdConteudo)
+        dfquest['prox_feed'] = dfquest['prox_feed'].astype(int)
+
+        ## Qtd de conteúdos por tipo
+        qtdTexto = dfquest['prox_feed'][dfquest['tipo_alternativa'] == "texto"].iloc[0]
+        qtdTeste = dfquest['prox_feed'][dfquest['tipo_alternativa'] == "teste"].iloc[0]
+        qtdAudio = dfquest['prox_feed'][dfquest['tipo_alternativa'] == "audio"].iloc[0]
+        qtdVideo = dfquest['prox_feed'][dfquest['tipo_alternativa'] == "video"].iloc[0]
 
 
-        ##[['texto', 8.0],
-        #  ['teste', 6.0],
-        #  ['audio', 4.0],
-        #  ['video', 2.0]]
+        ########################### Esquema de seleção de conteúdo no banco #################################
+        #####################################################################################################
 
-        # query = 'select * from shae_db.conteudo'
-        # cursor.execute(query)
-        # listaConteudos = cursor.fetchall()
-        # print(listaConteudos)
-
-        ## saida json
-        # [
-        #     1,
-        #     "video Algebra",
-        #     "video",
-        #     1,
-        #     1,
-        #     null,
-        #     "https://www.youtube.com/watch?v=i7MZpiRht2E"
-        # ]
-
-        ## valor conteudos
-        qtdTexto = int(qtdConteudo[0][1])
-        qtdTeste = int(qtdConteudo[1][1])
-        qtdAudio = int(qtdConteudo[2][1])
-        qtdVideo = int(qtdConteudo[3][1])
-
-
-
-        # print('||||||||||||||||||||||||')
-        # print(qtdTexto)
-        # print(qtdTeste)
-        # print(qtdAudio)
-        # print(qtdVideo)
-        # print('||||||||||||||||||||||||')
-
-        # resp_questionario = pd.read_sql(query,conn)
-        # resp_questionario.columns = ["aluno_id","tipo","peso"]
-        # print(resp_questionario)
-
-
-        # query = 'select * from shae_db.conteudo'
         
         query = 'select * from shae_db.v_obterConteudosComProfessores'
 
         dfConteudos = pd.read_sql(query, conn)
         dfConteudos.columns = ["idConteudo", "descricao", "tipo", "ordem", "idTopico", "descricao_texto", "url","id_especialista","nome_especialista","user_tipo"]
+        
         conteudosTexto = dfConteudos.where(dfConteudos.tipo == "texto").dropna(subset=["idConteudo"]).head(qtdTexto)
-        conteudosTeste = dfConteudos.where(dfConteudos.tipo == "teste").dropna(subset=["idConteudo"]).head(qtdTeste)
-        conteudosAudio = dfConteudos.where(dfConteudos.tipo == "audio").dropna(subset=["idConteudo"]).head(qtdAudio)
+        conteudosTeste = dfConteudos.where(dfConteudos.tipo == "questionario").dropna(subset=["idConteudo"]).head(qtdTeste)
+        conteudosAudio = dfConteudos.where(dfConteudos.tipo == "imagem").dropna(subset=["idConteudo"]).head(qtdAudio)
         conteudosVideo = dfConteudos.where(dfConteudos.tipo == "video").dropna(subset=["idConteudo"]).head(qtdVideo)
 
-        print(conteudosTexto)
-        print("||||||||||||||||||||||||||")
-        print(conteudosTeste)
-        print("||||||||||||||||||||||||||")
-        print(conteudosAudio)
-        print("||||||||||||||||||||||||||")
-        print(conteudosVideo)
+        ### JSON com a relação de contéudos
+        conteudosFiltrados = pd.concat([conteudosTexto, conteudosVideo, conteudosTeste, conteudosAudio])
+        conteudosFiltrados = conteudosFiltrados.sample(frac=1)
 
-        conteudosFiltrados = pd.concat([conteudosTexto, conteudosVideo])
-        print("||||||||||||||||||||||||||")
         print(conteudosFiltrados)
+        for indice in conteudosFiltrados.index:
+            query = """INSERT INTO shae_db.ultimo_feed (id_feed, id_aprendiz, id_conteudo, consumido)
+                    VALUES({0}, {1}, {2}, false);""".format(1, user_id, conteudosFiltrados["idConteudo"][indice])
+            cursor.execute(query)
+            conn.commit()
 
         return conteudosFiltrados.to_json(orient="records", force_ascii=False)
 
@@ -228,7 +176,7 @@ def feed(user_id, user_tipo):
         #####################################################################################################
 
         feed_disp = pd.DataFrame({'id_conteudos': conteudosFiltrados.idConteudo,
-                                  'tipo': conteudosFiltrados.tipo})
+                                  'tipo': conteudosFiltrados.tipo}) 
 
         feed_disp_arm = pd.DataFrame({'qt_disp': feed_disp.groupby(['tipo'])['tipo'].size()}).reset_index()
 
@@ -274,11 +222,11 @@ def feed(user_id, user_tipo):
         conteudos_consumo = feed_disp_consumido['id_conteudos'].loc[feed_disp_consumido['consumo'] == 1]
         conteudos_consumo_list = conteudos_consumo.values.tolist()
 
-        query_prox_conteudo = 'select * from shae_db.conteudo'
+        query_prox_conteudo = 'select * from shae_db.v_obterConteudosComProfessores'
         query_prox_conteudo += ' where id_Conteudo not in ({0})'.format(str(conteudos_consumo_list)[1:-1])
 
         dfConteudos_prox = pd.read_sql(query_prox_conteudo, conn)
-        dfConteudos_prox.columns = ["idConteudo", "descricao", "tipo", "ordem", "idTopico", "descricao_texto", "url"]
+        dfConteudos_prox.columns = ["idConteudo", "descricao", "tipo", "ordem", "idTopico", "descricao_texto", "url","id_especialista","nome_especialista","user_tipo"]
 
         qtdtexto_prox = (merge_feed['prox_feed'].loc[merge_feed['tipo'] == "texto"]).tolist()
         qtdtexto_prox = int(float(str(qtdtexto_prox)[1:-1]))
